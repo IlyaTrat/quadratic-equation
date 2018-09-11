6ed667181fbcf45c3b0e392d65b98ff781a9ef58
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-11 22:06:57 +0300

    Done with home work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let split = equation.split(' ');
+  let a, b, c;
+  a = split[0];
+  split[3] == '-' ? b = 0 - split[4] : b = Number(split[4]);
+  split[7] == '-' ? c = 0 - split[8] : c = Number(split[8]);
+  return solver(a, b ,c);
+}
+
+function solver(a, b, c) {
+  let x1, x2;
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if(d > 0) {
+    x1 = Math.round((-1 * b + Math.sqrt(d)) / (2 * a));
+    x2 = Math.round((-1 * b - Math.sqrt(d)) / (2 * a));
+    return [x1, x2].sort((a, b) => a - b);
+  }
+  else if (d == 0) {
+    x1 = x2 = -1 * Math.round(b / (2 * a));
+    return [x1, x2].sort((a, b) => a - b);
+  }
+  else {
+    return 'not a simple answer'
+  }
 }
